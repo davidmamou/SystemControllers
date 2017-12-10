e99e7a11e7b17006679e29fd7caa5d98a4f2c712
David Mamou <david_mamou@live.fr>
2017-12-10 14:32:14 +0200

    Initial Commit

---
+++ SystemControllers/Controllers/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SystemControllers
+//
+//  Created by Adrien Meyer on 10/12/2017.
+//  Copyright © 2017 Developer.Institute. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+//MES GROSSES COUILLLES
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
